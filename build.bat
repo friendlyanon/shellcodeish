7f55fe3da649c4120f1ac86906bfc9ef38399e34
friendlyanon <friendlyanon@users.noreply.github.com>
2024-05-27 21:55:49 +0200

    Add a CFF Explorer script to remove .pdata section

--- build.bat
+++ build.bat
@@ -26,10 +26,19 @@ if not %errorlevel%==0 exit /b %errorlevel%
 set warnings=/w14165 /w44242 /w44254 /w34287 /w44296 /w44365 /w44388 /w44464 /w14545 /w14546 /w14547 /w14549 /w14555 /w34619 /w44774 /w44777 /w24826 /w14905 /w14906 /w14928 /W4
 set conformance=/utf-8 /std:c17 /permissive- /volatile:iso /Zc:inline /we4213
 set clargs=/nologo /GS- /Gs1000000000 /O2 /diagnostics:caret %warnings% %conformance% %cflags%
-set linkargs=shellcodeish%format%.obj /machine:%machine% /out:shellcodeish%format%.exe /subsystem:console /stub:stub.bin /ignore:4060 /emittoolversioninfo:no /entry:pre_entry /opt:icf /opt:ref /emitpogophaseinfo /fixed%ldflags%
+set linkargs=shellcodeish%format%.obj /machine:%machine% /out:shellcodeish%format%.exe /subsystem:console /stub:stub.bin /ignore:4060 /emittoolversioninfo:no /entry:pre_entry /opt:icf /opt:ref /emitpogophaseinfo /fixed /safeseh:no%ldflags%
 
 cl %clargs% main.c /link %linkargs%
 if not %errorlevel%==0 exit /b %errorlevel%
+
+if %format%==64 (
+  if exist cff_error.txt del cff_error.txt
+  call "C:\Program Files\NTCore\Explorer Suite\CFF Explorer.exe" remove_pdata.cff shellcodeish64.exe
+  if exist cff_error.txt (
+    type cff_error.txt >2
+    exit /b 1
+  )
+)
 
 endlocal
 
