d63eda5886e9478bb7034b566812d764d6480536
friendlyanon <friendlyanon@users.noreply.github.com>
2024-11-20 14:29:26 +0100

    Use `/Wall` for extreme strictness

--- build.bat
+++ build.bat
@@ -3,10 +3,10 @@
 @echo off
 
 set now=%time:,=.%
-echo Start build: %now: =0%>&2
+>&2 echo Start build: %now: =0%
 call :build %*
 set now=%time:,=.%
-echo Finish build: %now: =0%>&2
+>&2 echo Finish build: %now: =0%
 exit /b %errorlevel%
 
 :build
@@ -28,7 +28,7 @@ if "%VSCMD_ARG_TGT_ARCH%" == "" (
   call "!VCVARS!" -arch=%arch% -host_arch=amd64 -no_logo
   if not !errorlevel! == 0 exit /b !errorlevel!
 ) else if not !VSCMD_ARG_TGT_ARCH! == %machine% (
-  echo vcvars has already been initialized for !VSCMD_ARG_TGT_ARCH!, but the build is targeting %machine%>&2
+  >&2 echo vcvars has already been initialized for !VSCMD_ARG_TGT_ARCH!, but the build is targeting %machine%
   exit /b 1
 )
 
@@ -49,8 +49,8 @@ echo shellcodeish%format%.asm
 nasm.exe -f win%format% shellcodeish%format%.asm
 if not %errorlevel% == 0 exit /b %errorlevel%
 
-set warnings=/w14165 /w44242 /w44254 /w34287 /w44296 /w44365 /w44388 /w44464 /w14545 /w14546 /w14547 /w14549 /w14555 /w34619 /w44774 /w44777 /w24826 /w14905 /w14906 /w14928 /W4
-set conformance=/utf-8 /std:c17 /permissive- /volatile:iso /Zc:inline /we4213
+set warnings=/Wall /wd4711
+set conformance=/utf-8 /std:c17 /permissive- /volatile:iso /Zc:inline
 set clargs=/nologo /GS- /Gs1000000000 /O2 /diagnostics:caret /I. %warnings% %conformance% %cflags%
 set linkargs=shellcodeish%format%.obj import%format%.obj /machine:%machine% /out:shellcodeish%format%.exe /subsystem:console /stub:stub.bin /ignore:4060 /entry:pre_entry /opt:icf /opt:ref /emittoolversioninfo:no /emitpogophaseinfo /fixed /safeseh:no /align:16 /ignore:4108
 
@@ -68,13 +68,13 @@ if %format% == 64 (
   if not !errorlevel! == 0 exit /b !errorlevel!
 )
 
-endlocal & exit /b 0
+exit /b 0
 
 :cff
 "C:\Program Files\NTCore\Explorer Suite\CFF Explorer.exe" %*
 if not exist cff_error.txt exit /b 0
 
-type cff_error.txt>&2
+>&2 type cff_error.txt
 exit /b 1
 
 @end
